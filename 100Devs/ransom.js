c3cfeab5a6c6303c3a4997ab9b0166251dc914c4
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2022-12-15 17:36:55 -0500

    Added 100 devs stuff

--- 100Devs/ransom.js
+++ 100Devs/ransom.js
@@ -11,8 +11,6 @@ const ransomNote = (ransomString, magazine) => {
     // Split both strings into arrays
     ransom_arr = ransomString.split(" ");
     magazine_arr = magazine.split(" ");
-
-    // See if ransomString as an array is a subset of magazine as an array, return true
     
     // Idea 1:
     // Make a hash map of counts to each unique word, e.g: { "Lorem": 1, "in": 2, ...}
@@ -43,24 +41,25 @@ const ransomNote = (ransomString, magazine) => {
         }
     }
 
-    // console.log("\n")
     // If the count for each item in ransomString matches the counts in magazine, return true, otherwise return falses 
     for (word of ransom_arr) {
         // if the every value for the word in ransomDict = respective value in magazineDict, return true
-        const debug = word + " in ransomString: " + ransomDict[word] + ", " + word + " in magazine: " +  magazineDict[word]
-        // console.log(debug); 
         // so we only need to return false when we find there are MORE occurences of the word in RANSOMSTRING than in the MAGAZINE:
-        // Also return false if not found in magazine
+        // Also return false if not found in magazine i.e. === undefined
         if (ransomDict[word] > magazineDict[word] || magazineDict[word] === undefined) {
             return false;
         }
     }
+
     // and we can return true when we've passed all the tests
     return true;
+
+    // Time complexity = O(9N + 4) --> Polynomial
+    // Memory complexity = O(4) --> Constant
 }
 
 
-// TEST BELOW
+// TESTS BELOW
 const magazine =
  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
@@ -71,6 +70,3 @@ console.log("Should return false", ransomNote("sit ad est love", magazine));
 console.log("Should return true", ransomNote("sit ad est sint in in", magazine));
 
 console.log("Should return false", ransomNote("sit ad est sint in in in in", magazine));
-
-// console.log(ransomNote("egg",magazine));
-// module.exports = ransom;
