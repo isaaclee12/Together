b18ddfba00a7eae679121bebcebebe873f88893f
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2022-12-21 00:15:01 -0500

    codewars

---
+++ codewars/javascript-typescript/4kyu/rectangleRotation.tsx
@@ -0,0 +1,18 @@
+// was tired, need paper, noped out
+
+export function rectangleRotation(a: number, b: number): number {
+  // parameters: two ints, a (top-left and bottom right) and b (top-right and bottom-left)
+  // returns: number of points within a rectangle of width a and length b in a 45 degree rotated rectangle on an xy graph
+  let output:number = 0;
+  
+  /* Brainstorming:
+  Let's start with the smallest possible examples:
+  a=0,b=0 => 0
+  a=1,b-0 or vice versa => 0
+  a=1,b=1 => 1
+  a=1,b=2 => 1
+  a=2,b=2 => 2
+  oh god i need a piece of paper
+  */
+  
+}
---
+++ codewars/javascript-typescript/4kyu/stripComments.js
@@ -0,0 +1,10 @@
+
+
+// another solution:
+function solution(input, markers) {
+  return input.split('\n').map(
+    line => markers.reduce(
+      (line, marker) => line.split(marker)[0].trim(), line
+    )
+  ).join('\n')
+}
